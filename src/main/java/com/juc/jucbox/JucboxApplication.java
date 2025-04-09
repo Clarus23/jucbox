1fca5aff50534a023145adbf61fbe32aca6ff069
Clarus23 <dnjs3023@gmail.com>
2025-04-09 17:33:26 +0900

    project init

---
+++ src/main/java/com/juc/jucbox/JucboxApplication.java
@@ -0,0 +1,13 @@
+package com.juc.jucbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JucboxApplication.class, args);
+	}
+
+}
